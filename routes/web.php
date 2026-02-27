ea163ff96807bc7184950c9f89e6b3b85a47c4b6
PedroCosta083 <pedroh.costa083@gmail.com>
2026-02-27 02:24:01 -0300

    refactor(routes): padroniza middlewares de permissão em web.php

--- routes/web.php
+++ routes/web.php
@@ -29,31 +29,35 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
     
+    // Resources usam authorizeResource() nos controllers
     Route::resource('products', ProductController::class);
-    Route::resource('orders', OrderController::class)->except(['create', 'store', 'destroy']);
-    Route::put('orders/{order}/status', [OrderController::class, 'updateStatus'])->name('orders.updateStatus')->middleware('permission:orders.update');
     Route::resource('categories', CategoryController::class);
     Route::resource('tags', TagController::class);
+    Route::resource('orders', OrderController::class)->except(['create', 'store', 'destroy']);
+    Route::put('orders/{order}/status', [OrderController::class, 'updateStatus'])->name('orders.updateStatus');
     
-    Route::prefix('reports')->name('reports.')->middleware('permission:products.view')->group(function () {
+    // Reports - requer permissão products.view
+    Route::prefix('reports')->name('reports.')->middleware('can:products.view')->group(function () {
         Route::get('/', [App\Http\Controllers\Web\ReportController::class, 'index'])->name('index');
         Route::get('/low-stock', [App\Http\Controllers\Web\ReportController::class, 'lowStock'])->name('low-stock');
         Route::get('/stock-movements', [App\Http\Controllers\Web\ReportController::class, 'stockMovements'])->name('stock-movements');
         Route::get('/revenue', [App\Http\Controllers\Web\ReportController::class, 'revenue'])->name('revenue');
     });
     
-    Route::prefix('notifications')->name('notifications.')->middleware('permission:products.view')->group(function () {
+    // Notifications - requer permissão products.view
+    Route::prefix('notifications')->name('notifications.')->middleware('can:products.view')->group(function () {
         Route::get('/', [App\Http\Controllers\Web\NotificationController::class, 'index'])->name('index');
         Route::post('/{id}/read', [App\Http\Controllers\Web\NotificationController::class, 'markAsRead'])->name('mark-read');
         Route::post('/read-all', [App\Http\Controllers\Web\NotificationController::class, 'markAllAsRead'])->name('mark-all-read');
         Route::get('/unread-count', [App\Http\Controllers\Web\NotificationController::class, 'getUnreadCount'])->name('unread-count');
     });
     
-    Route::resource('roles', RoleController::class)->middleware('permission:roles.view');
-    Route::resource('permissions', PermissionController::class)->only(['index', 'store', 'destroy'])->middleware('permission:permissions.view');
-    Route::resource('users', UserRoleController::class)->only(['index'])->middleware('permission:users.view');
-    Route::get('users/{user}/edit-roles', [UserRoleController::class, 'edit'])->name('users.edit-roles')->middleware('permission:roles.update');
-    Route::put('users/{user}/roles', [UserRoleController::class, 'update'])->name('users.update-roles')->middleware('permission:roles.update');
+    // Roles & Permissions - requer permissões específicas
+    Route::resource('roles', RoleController::class)->middleware('can:roles.view');
+    Route::resource('permissions', PermissionController::class)->only(['index', 'store', 'destroy'])->middleware('can:permissions.view');
+    Route::resource('users', UserRoleController::class)->only(['index'])->middleware('can:users.view');
+    Route::get('users/{user}/edit-roles', [UserRoleController::class, 'edit'])->name('users.edit-roles')->middleware('can:roles.update');
+    Route::put('users/{user}/roles', [UserRoleController::class, 'update'])->name('users.update-roles')->middleware('can:roles.update');
 });
 
 require __DIR__.'/settings.php';
